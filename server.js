771574b01a4a9338b49a5386cebd418194f9b54d
Amir Hossein <nasryzdany0879@gmail.com>
2026-08-24 23:43:27 +0330

    Add secure admin login

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PRODUCTS_FILE = path.join(ROOT, "products.json");
 */
 
 const ADMIN_PASSWORD =
-    process.env.ADMIN_PASSWORD || "Nanak1405";
+    process.env.ADMIN_PASSWORD || "نانک1405";
 
 /*
 ==================================================
